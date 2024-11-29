f98fb31a4238060df098ae1b410b4b530e1ff4ae
lillyanrezende <lillyanaloisemkt@gmail.com>
2024-11-29 13:42:41 +0000

    acrescentado data e hora no dicionario

--- chat_bot.py
+++ chat_bot.py
@@ -25,6 +25,8 @@ def obter_resposta(texto: str) -> str:
         'como estás': 'Estou bem, obrigado!',
         'como te chamas': 'O meu nome é: Bot :)',
         'tempo': 'Está um dia de sol!',
+        ('horas'): f'São: {datetime.now():%H:%M} horas',
+        ('data'): f'Hoje é dia: {datetime.now():%d-%m-%Y}',
         ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...'
     }
 
@@ -35,11 +37,7 @@ def obter_resposta(texto: str) -> str:
         elif chave in comando:
             return resposta
 
-    if 'horas' in comando:
-        return f'São: {datetime.now():%H:%M} horas'
-
-    if 'data' in comando:
-        return f'Hoje é dia: {datetime.now():%d-%m-%Y}'
+    
 
     return f'Desculpa, não entendi a questão! {texto}'
 
@@ -54,7 +52,7 @@ def chat() -> None:
         user_input: str = input('Tu: ')
         resposta: str = obter_resposta(user_input)
         print(f'Bot: {resposta}')
-
+        
         if resposta == 'Gostei de falar contigo! Até breve...':
             break
 
